0437ee6ec41ec9bb30b43e8ce36028c2d9988187
Tony Schaffert <antonio.schaffert@gmail.com>
2021-12-12 20:10:51 -0500

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <section class="section">
+      Welcome to our Dad Jokes Website.
+
+      <div>
+        <b-button type="is-primary is-light" @click="search(1)">Load me tha funny Joke!!!</b-button>
+      </div>
+    </section>
+
+    <section v-if="joke">
+      <div>
+        {{ joke.question }}
+      </div>
+      <div>
+        <b-button type="is-secondary is-light" @click="show_answer = !show_answer">Tell me!</b-button>        
+      </div>
+      <div v-if="show_answer">
+        {{ joke.answer }}
+      </div>
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        joke: {},
+        show_answer: false
+      }
+    },
+    methods: {
+      reset() {
+        this.joke = {};
+        show_answer: false;
+      },
+      async search(page=1) {
+        this.reset();
+        const response = await this.$axios.$get("https://hh-jokes-api.herokuapp.com/random")
+        console.log(response)
+        this.joke = response;
+      },
+    }
+}
+</script>
---
+++ pages/jokes.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <section class="section">
+      All Jokes
+    </section>
+    <section class="section">
+      <b-button type="is-primary is-light" @click="search(1)">Get Jokes</b-button>
+    </section>
+
+    <section>
+        <!-- Results -->
+        <pre>
+          {{ jokes }}
+        </pre>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        jokes: [],
+      }
+    },
+    methods: {
+      async search(page=1) {
+        const response = await this.$axios.$get("https://hh-jokes-api.herokuapp.com/all-jokes")
+        console.log(response)
+        this.jokes = response
+      },
+    }
+}
+</script>
